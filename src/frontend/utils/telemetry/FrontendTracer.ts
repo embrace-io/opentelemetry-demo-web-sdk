cf2bdafae2a1b45e1483a4982492001a1e191be2
Gregorio Gerardi <gregorionicolasgerardi@gmail.com>
2025-01-21 10:31:20 -0300

    integrate embrace sdk into otel demo
    (cherry picked from commit 58762352b6c1b6066a145c0957e85531398ccfdf)

--- src/frontend/utils/telemetry/FrontendTracer.ts
+++ src/frontend/utils/telemetry/FrontendTracer.ts
@@ -2,17 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { CompositePropagator, W3CBaggagePropagator, W3CTraceContextPropagator } from '@opentelemetry/core';
-import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
-import { Resource, browserDetector } from '@opentelemetry/resources';
+import { browserDetector, Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { SessionIdProcessor } from './SessionIdProcessor';
 import { detectResourcesSync } from '@opentelemetry/resources/build/src/detect-resources';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { sdk } from '@embraceio/embrace-web-sdk';
 
+const SAMPLE_APP_ID = 'wkugd';
 const {
   NEXT_PUBLIC_OTEL_SERVICE_NAME = '',
   NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = '',
@@ -26,43 +25,36 @@ const FrontendTracer = (collectorString: string) => {
 
   const detectedResources = detectResourcesSync({ detectors: [browserDetector] });
   resource = resource.merge(detectedResources);
-  const provider = new WebTracerProvider({ resource });
-
-  provider.addSpanProcessor(new SessionIdProcessor());
-
-  provider.addSpanProcessor(
-    new BatchSpanProcessor(
-      new OTLPTraceExporter({
-        url: NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT || collectorString || 'http://localhost:4318/v1/traces',
-      }),
-      {
-        scheduledDelayMillis: 500,
-      }
-    )
+  const spanProcessor = new BatchSpanProcessor(
+    new OTLPTraceExporter({
+      url: NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT || collectorString || 'http://localhost:4318/v1/traces',
+    }),
+    {
+      scheduledDelayMillis: 500,
+    }
   );
 
   const contextManager = new ZoneContextManager();
 
-  provider.register({
-    contextManager,
-    propagator: new CompositePropagator({
-      propagators: [new W3CBaggagePropagator(), new W3CTraceContextPropagator()],
-    }),
+  const propagator = new CompositePropagator({
+    propagators: [new W3CBaggagePropagator(), new W3CTraceContextPropagator()],
   });
 
-  registerInstrumentations({
-    tracerProvider: provider,
-    instrumentations: [
-      getWebAutoInstrumentations({
-        '@opentelemetry/instrumentation-fetch': {
-          propagateTraceHeaderCorsUrls: /.*/,
-          clearTimingResources: true,
-          applyCustomAttributesOnSpan(span) {
-            span.setAttribute('app.synthetic_request', IS_SYNTHETIC_REQUEST);
-          },
+  sdk.initSDK({
+    resource: resource,
+    appID: SAMPLE_APP_ID,
+    contextManager,
+    propagator,
+    spanProcessors: [spanProcessor],
+    instrumentations: getWebAutoInstrumentations({
+      '@opentelemetry/instrumentation-fetch': {
+        propagateTraceHeaderCorsUrls: /.*/,
+        clearTimingResources: true,
+        applyCustomAttributesOnSpan(span) {
+          span.setAttribute('app.synthetic_request', IS_SYNTHETIC_REQUEST);
         },
-      }),
-    ],
+      },
+    }),
   });
 };
 
